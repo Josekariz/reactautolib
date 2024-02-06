83c3181516d0b929dba422f8fbe2b4added703c4
Joseph <sejokarizz@gmail.com>
2024-02-06 19:55:25 +0300

    create login and sign up form

--- frontend/src/auth/LogIn.jsx
+++ frontend/src/auth/LogIn.jsx
@@ -1,15 +1,58 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import backg from "../assets/authbg.jpg";
-import React from "react";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [activeTab, setActiveTab] = useState("login");
+
+  const navigateToSignUp = () => {
+    setActiveTab("signup");
+    navigate("/sign_up");
+  };
+
   return (
     <div
       className="min-h-screen flex items-center justify-center bg-cover"
       style={{ backgroundImage: `url(${backg})` }}
     >
-      <div className="bg-black-100 glass p-8 rounded-lg shadow-lg">
-        <h2 className="text-2xl font-semibold mb-4">Login</h2>
-        {/* Your login form goes here */}
+      <div className="bg-black p-8 rounded-lg shadow-lg text-white">
+        <div className="flex justify-center mb-4">
+          <button
+            className={`text-2xl font-semibold mb-4 mr-4 ${
+              activeTab === "login" ? "text-blue-500" : ""
+            }`}
+            disabled={activeTab === "login"}
+            onClick={() => setActiveTab("login")}
+          >
+            Login
+          </button>
+          <button
+            className={`text-2xl font-semibold mb-4 ${
+              activeTab === "signup" ? "text-blue-500" : ""
+            }`}
+            disabled={activeTab === "signup"}
+            onClick={navigateToSignUp}
+          >
+            Sign Up
+          </button>
+        </div>
+
+        <div>
+          <input
+            className="w-full p-2 mb-4 rounded text-black"
+            type="email"
+            placeholder="Email Address"
+          />
+          <input
+            className="w-full p-2 mb-4 rounded text-black"
+            type="password"
+            placeholder="Password"
+          />
+          <button className="w-full bg-black text-white text-2xl font-semibold p-2 rounded border-solid border-2 border-sky-500 hover:bg-blue-500">
+            Log In
+          </button>
+        </div>
       </div>
     </div>
   );
--- frontend/src/auth/SignUp.jsx
+++ frontend/src/auth/SignUp.jsx
@@ -1,16 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import backg from "../assets/authbg.jpg";
 
 const SignUp = () => {
+  const navigate = useNavigate();
+  const [activeTab, setActiveTab] = useState("signup");
+
+  const navigateToLogin = () => {
+    setActiveTab("login");
+    navigate("/login");
+  };
+
   return (
     <div
       className="min-h-screen flex items-center justify-center bg-cover"
       style={{ backgroundImage: `url(${backg})` }}
     >
-      <div className="bg-black-100 glass p-8 rounded-lg shadow-lg">
-        <h2 className="text-2xl font-semibold mb-4">Sign Up</h2>
-        {/* Your signup form goes here */}
-        
+      <div className="bg-black p-8 rounded-lg shadow-lg text-white">
+        <div className="flex justify-center mb-4">
+          <button
+            className={`text-2xl font-semibold mb-4 mr-4 ${
+              activeTab === "login" ? "text-blue-500" : ""
+            }`}
+            disabled={activeTab === "login"}
+            onClick={navigateToLogin}
+          >
+            Login
+          </button>
+          <button
+            className={`text-2xl font-semibold mb-4 ${
+              activeTab === "signup" ? "text-blue-500" : ""
+            }`}
+            disabled={activeTab === "signup"}
+          >
+            Sign Up
+          </button>
+        </div>
+
+        <div>
+          <input
+            className="w-full p-2 mb-4 rounded text-black"
+            type="text"
+            placeholder="First or Last Name"
+          />
+          <input
+            className="w-full p-2 mb-4 rounded text-black"
+            type="email"
+            placeholder="Email Address"
+          />
+          <input
+            className="w-full p-2 mb-4 rounded text-black"
+            type="password"
+            placeholder="Password"
+          />
+          <button className="w-full bg-black text-white text-2xl font-semibold p-2 rounded border-solid border-2 border-sky-500 hover:bg-blue-500">
+            Sign Up
+          </button>
+        </div>
       </div>
     </div>
   );
