8b32bb3a0d7f991529f9cefce2e39c4c63a1a3a3
Joseph <sejokarizz@gmail.com>
2024-02-06 16:28:44 +0300

    update fav icon and metadata

--- frontend/src/auth/LogIn.jsx
+++ frontend/src/auth/LogIn.jsx
@@ -7,7 +7,7 @@ const Login = () => {
       className="min-h-screen flex items-center justify-center bg-cover"
       style={{ backgroundImage: `url(${backg})` }}
     >
-      <div className="bg-white p-8 rounded-lg shadow-lg">
+      <div className="bg-black-100 glass p-8 rounded-lg shadow-lg">
         <h2 className="text-2xl font-semibold mb-4">Login</h2>
         {/* Your login form goes here */}
       </div>
--- frontend/src/auth/SignUp.jsx
+++ frontend/src/auth/SignUp.jsx
@@ -7,9 +7,10 @@ const SignUp = () => {
       className="min-h-screen flex items-center justify-center bg-cover"
       style={{ backgroundImage: `url(${backg})` }}
     >
-      <div className="bg-white p-8 rounded-lg shadow-lg">
+      <div className="bg-black-100 glass p-8 rounded-lg shadow-lg">
         <h2 className="text-2xl font-semibold mb-4">Sign Up</h2>
         {/* Your signup form goes here */}
+        
       </div>
     </div>
   );
