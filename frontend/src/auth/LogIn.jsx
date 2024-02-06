790c198264367ee70a6d16532862aa6007031201
Joseph <sejokarizz@gmail.com>
2024-02-06 20:39:53 +0300

    impliment login logic

--- frontend/src/auth/LogIn.jsx
+++ frontend/src/auth/LogIn.jsx
@@ -5,10 +5,56 @@ import backg from "../assets/authbg.jpg";
 const Login = () => {
   const navigate = useNavigate();
   const [activeTab, setActiveTab] = useState("login");
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
 
   const navigateToSignUp = () => {
     setActiveTab("signup");
     navigate("/sign_up");
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setLoginData({
+      ...loginData,
+      [name]: value,
+    });
+  };
+
+
+  //Login logic
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const { email, password } = loginData; // Extract email and password from state
+
+    try {
+      const response = await fetch("http://localhost:4000/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        // Handle successful login
+        // For example, storing the JWT in localStorage
+        localStorage.setItem("token", data.token);
+        // Redirect or update the UI
+        navigate("/"); // Replace with your success route
+      } else {
+        // Handle errors
+        console.error("Login failed");
+      }
+    } catch (error) {
+      // Handle network errors
+      console.error("Network error: ", error);
+    }
   };
 
   return (
@@ -38,18 +84,29 @@ const Login = () => {
           </button>
         </div>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
-            className="w-full p-2 mb-4 rounded text-black"
+            className="w-full p-2 mb-4 rounded text-white"
             type="email"
             placeholder="Email Address"
+            name="email"
+            required
+            value={loginData.email}
+            onChange={handleInputChange}
           />
           <input
-            className="w-full p-2 mb-4 rounded text-black"
+            className="w-full p-2 mb-4 rounded text-white"
             type="password"
             placeholder="Password"
+            name="password"
+            required
+            value={loginData.password}
+            onChange={handleInputChange}
           />
-          <button className="w-full bg-black text-white text-2xl font-semibold p-2 rounded border-solid border-2 border-sky-500 hover:bg-blue-500">
+          <button
+            type="submit"
+            className="w-full bg-black text-white text-2xl font-semibold p-2 rounded border-solid border-2 border-sky-500 hover:bg-blue-500"
+          >
             Log In
           </button>
         </form>
--- frontend/src/auth/SignUp.jsx
+++ frontend/src/auth/SignUp.jsx
@@ -5,10 +5,28 @@ import backg from "../assets/authbg.jpg";
 const SignUp = () => {
   const navigate = useNavigate();
   const [activeTab, setActiveTab] = useState("signup");
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
   const navigateToLogin = () => {
     setActiveTab("login");
     navigate("/login");
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
   };
 
   return (
@@ -37,23 +55,35 @@ const SignUp = () => {
           </button>
         </div>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
-            className="w-full p-2 mb-4 rounded text-black"
+            className="w-full p-2 mb-4 rounded text-white"
             type="text"
+            required
             placeholder="First or Last Name"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
           />
           <input
-            className="w-full p-2 mb-4 rounded text-black"
+            className="w-full p-2 mb-4 rounded text-white"
             type="email"
             placeholder="Email Address"
+            required
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
           />
           <input
-            className="w-full p-2 mb-4 rounded text-black"
+            className="w-full p-2 mb-4 rounded text-white"
             type="password"
+            required
             placeholder="Password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
           />
-          <button className="w-full bg-black text-white text-2xl font-semibold p-2 rounded border-solid border-2 border-sky-500 hover:bg-blue-500">
+          <button type="submit" className="w-full bg-black text-white text-2xl font-semibold p-2 rounded border-solid border-2 border-sky-500 hover:bg-blue-500">
             Sign Up
           </button>
         </form>
