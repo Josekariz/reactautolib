8d0d2259db022a43158822aa451830d666acaa99
Joseph <sejokarizz@gmail.com>
2024-02-11 19:59:36 +0300

    add firebase variables

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,12 @@ import {getStorage} from 'firebase/storage'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCdnLF0pte8_KsWO_mDAdBfaP6W81M_uYw",
-  authDomain: "autolib-605ff.firebaseapp.com",
-  projectId: "autolib-605ff",
-  storageBucket: "autolib-605ff.appspot.com",
-  messagingSenderId: "685597849",
-  appId: "1:685597849:web:3e7155f81fadc51618c408"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
